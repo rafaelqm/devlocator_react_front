62025fa8217ef5aea7d1df5d0799f29ef8e751cf
Rafael Querino Moreira <rafaelqm@gmail.com>
2020-01-19 12:33:00 -0300

    Done the layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./global.css";
+import "./Sidebar.css";
+import "./Main.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/4494881?s=460&v=4"
+                alt="Avatar do Usuário"
+              />
+              <div className="user-info">
+                <strong>Rafael Querino</strong>
+                <span>PHP, JavaScript, ReactJs, Node.Js</span>
+              </div>
+            </header>
+            <p>
+              Father, Husband, Developer, Entrepreneur, Libertarian, always
+              trying to be better than yesterday
+            </p>
+            <a href="https://github.com/rafaelqm">Acessar perfil no GitHub</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/4494881?s=460&v=4"
+                alt="Avatar do Usuário"
+              />
+              <div className="user-info">
+                <strong>Rafael Querino</strong>
+                <span>PHP, JavaScript, ReactJs, Node.Js</span>
+              </div>
+            </header>
+            <p>
+              Father, Husband, Developer, Entrepreneur, Libertarian, always
+              trying to be better than yesterday
+            </p>
+            <a href="https://github.com/rafaelqm">Acessar perfil no GitHub</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/4494881?s=460&v=4"
+                alt="Avatar do Usuário"
+              />
+              <div className="user-info">
+                <strong>Rafael Querino</strong>
+                <span>PHP, JavaScript, ReactJs, Node.Js</span>
+              </div>
+            </header>
+            <p>
+              Father, Husband, Developer, Entrepreneur, Libertarian, always
+              trying to be better than yesterday
+            </p>
+            <a href="https://github.com/rafaelqm">Acessar perfil no GitHub</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars3.githubusercontent.com/u/4494881?s=460&v=4"
+                alt="Avatar do Usuário"
+              />
+              <div className="user-info">
+                <strong>Rafael Querino</strong>
+                <span>PHP, JavaScript, ReactJs, Node.Js</span>
+              </div>
+            </header>
+            <p>
+              Father, Husband, Developer, Entrepreneur, Libertarian, always
+              trying to be better than yesterday
+            </p>
+            <a href="https://github.com/rafaelqm">Acessar perfil no GitHub</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
